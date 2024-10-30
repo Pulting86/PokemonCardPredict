1a387a1881db69882c14d648aa77985e4ee8298f
OscarZ <oscar.haotian.ceb@immune.institute>
2024-10-30 23:57:46 +0100

    Progreso en el proyecto

---
+++ app/app.py
@@ -0,0 +1,16 @@
+# import gradio as gr
+#
+# def predecir(Nombre,Rareza,Antiguedad,Fullart,Holo,Foil,PSA,Idioma):
+#   pass
+#   return Precio
+#
+#
+# demo = gr.Interface(
+#     fn=predecir,
+#     inputs=["text",gr.Slider(value=1, minimum=1, maximum=10, step=1),gr.Slider(value=1996, minimum=1996, maximum=2024, step=1),gr.Radio(["Si", "No"], label="Fullart"),gr.Radio(["Si", "No"], label="Holo"),gr.Radio(["Si", "No"], label="Foil"),gr.Slider(value=1, minimum=1, maximum=10, step=1),gr.Radio(["Inglés", "Japonés"], label="Idioma"),],
+#     outputs=["text"],
+#     allow_flagging='never'
+# )
+# demo.launch()
+
+print("Hola")
